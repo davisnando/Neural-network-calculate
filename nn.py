e3b1534bfdacc8ace8f17fc88af013676b24f78b
Nando Davis <nandodavis@gmail.com>
2019-06-14 17:25:25 +0200

    Update neuraal netwerk

--- nn.py
+++ nn.py
@@ -1,18 +1,50 @@
 import json
+import numpy as np
+
 
 data = json.load(open("layer.json", "r"))
 
-inputs = {}
 
+def set_matrix(layer):
+    # creating matrix for the layer
+    # set empty matrix
+    matrix = np.zeros((layer["size_out"], layer["size_in"]))
+    # fill matrix with given weights
+    for key, weights in layer["weights"].items():
+        key = int(key)
+        for k, w in weights.items():
+            k = int(k)
+            w = float(w)
+            matrix[k - 1][key - 1] = w
+    return matrix
+
+
+def calculate_matrix(matrix, input):
+    output = []
+    # loop through every row of the matrix
+    for row in matrix:
+        # sum
+        s = 0
+        # save index of weight
+        index = 0
+        # loop through every matrix
+        for weight in row:
+            # add input * weight to sum
+            s += input[index] * weight
+            # increase index
+            index += 1
+        # add sum to output
+        output.append(s)
+    # return output
+    return output
+
+
+# set default input
+i = np.array([1, 1, 1, 1, 1])
+# loop through every layer of the matrix
 for key, layer in data.items():
-    for key, weight in layer["weights"].items():
-        total_sum = 0
-        for k, w in weight.items():
-            if k not in inputs:
-                inputs[k] = 1
-            total_sum += eval(w) * inputs[k]
-
-        inputs[key] = total_sum
-
-print(inputs)
-
+    # create a matrix
+    matrix = set_matrix(layer)
+    # calculate matrix
+    i = calculate_matrix(matrix, i)
+print(i)
